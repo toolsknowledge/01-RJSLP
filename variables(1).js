d9be4ff5e736724ea597b469751f568a5c396085
Samba <samba.pendela@gmail.com>
2021-05-11 23:04:27 +0530

    code

---
+++ variables(1).js
@@ -0,0 +1,22 @@
+//variables are used to store the data
+//Ex.  string  number  boolean    arrays   objects ........
+//we will declare the variables by using "var" / "let" / "const" keyword
+//"let" and "const" keyword introduced in "ES6".
+//variables should contain A-Z, a-z , 0-9 , $ and _
+//variables should not start with digits
+//Syntax:        var/let/const variablename = value;
+
+//DataTypes
+//1) string         2) number           3) boolean          4) undefined        5) null         6) bigint       7) Symbol       8) Object
+
+
+//string
+//collection of characters called as "string"
+//"" (double quotes) , ''(single quotes)    and ``(backtick)
+//``(backtick) operator introduced in "ES6"
+//``(backtick) operator also called as "template literal"
+//template literal, used to define the paragraphs
+
+
+
+
