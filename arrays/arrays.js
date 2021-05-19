d573258685b6f216173ee2f05b767d81d68a5317
Samba <samba.pendela@gmail.com>
2021-05-19 22:51:23 +0530

    arrays

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -161,25 +161,151 @@ for(let value of arr){
 */
 
 
+/*
+    //reduce()
+    //it is used to find the sum of array elements
+    console.log(
+        [1,2,3,4,5].reduce((firstValue,nextValue)=>{
+            return firstValue + nextValue;
+        })
+    );
+    //15
 
-//reduce()
-//it is used to find the sum of array elements
-console.log(
-    [1,2,3,4,5].reduce((firstValue,nextValue)=>{
-        return firstValue + nextValue;
-    })
-);
-//15
+    console.log(
+        [1,2,3,4,5].map((element,index)=>{
+            return element
+        }).filter((element,index)=>{
+            return element<=5;
+        }).reduce((firstValue,nextValue)=>{
+            return firstValue+nextValue;
+        })
+    );              //15
+*/
+
+
+
+/*
+    //reduceRight()
+    //used to find the summation from right to left
+    console.log(
+        ["AshokIT","to","Welcome"].reduceRight((firstValue,nextValue)=>{
+            return firstValue+" "+nextValue;
+        })
+    );              //Welcome to AshokIT
+*/
+
+
+/*
+    //findIndex()
+    //it is used to know the index of a particular element in array
+    let arr = [10,20,30,40,50,60,70,80,90,100];
+    console.log(
+        arr.findIndex((element,index)=>{
+            return element == 50;
+        })
+    );              //4
+
+    console.log(
+        arr.findIndex((element,index)=>{
+            return element == 100;
+        })
+    );      //9
+
+
+    console.log(
+        arr.findIndex((element,index)=>{
+            return element == 1000;
+        })
+    );      //-1
+
+
+    arr.splice(arr.findIndex((element,index)=>{
+        return element == 50
+    }),1);
+    console.log(arr);
+
+
+    arr.splice( arr.findIndex((element,index)=>{
+        return element == 100
+    }), 1 );
+    console.log(arr);
+
+
+    let arr1 = [
+        {"p_id":111,"p_name":"p_one","p_cost":10000},
+        {"p_id":222,"p_name":"p_two","p_cost":20000},
+        {"p_id":333,"p_name":"p_three","p_cost":30000},
+        {"p_id":444,"p_name":"p_four","p_cost":40000},
+        {"p_id":555,"p_name":"p_five","p_cost":50000}
+    ];
+    arr1.splice(arr1.findIndex((element,index)=>{
+        return element.p_id === 333
+    }),1);
+    console.log( arr1 );
+*/
+
+
+//indexOf()
+//it won't create indexes to duplicate elements
+let arr = [10,20,30,10,40,30,10];
+for(let value of arr){
+    console.log( arr.indexOf(value) );
+};
+
+
+let arr1 = [10,20,30,10,20,30];
+arr1.forEach((element,index)=>{
+    console.log(element,index,arr1.indexOf(element));
+});
+
+//10   0    0
+//20   1    1
+//30   2    2
+//10   3    0
+//20   4    1
+//30   5    2 
 
 console.log(
-    [1,2,3,4,5].map((element,index)=>{
-        return element
-    }).filter((element,index)=>{
-        return element<=5;
-    }).reduce((firstValue,nextValue)=>{
-        return firstValue+nextValue;
+    arr1.filter((element,index)=>{
+        return arr1.indexOf(element) === index;
     })
-);              //15
+);      //[ 10, 20, 30 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
