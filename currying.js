ac647e70feab4e50d6759f58de1cf7f3807befff
Samba <samba.pendela@gmail.com>
2021-06-29 22:30:06 +0530

    JS Expressions

---
+++ currying.js
@@ -0,0 +1,16 @@
+// const calc = (num1,num2,num3)=>{
+//     return num1+num2+num3;
+// };
+// console.log( calc(10,10,10) );
+
+
+// const calc = (num1)=>{
+//     return (num2)=>{
+//         return (num3)=>{
+//             return num1+num2+num3;
+//         }
+//     }
+// };
+// console.log( calc(10)(10)(10) );                //30
+
+
